3a3e09d055afb31ce6c8b6da5954d1260784a494
unknown <maallen@g.ucla.edu>
2025-02-16 16:55:51 -0800

    Added schema for CreateTask restAPI

--- backend/routers/tasks.py
+++ backend/routers/tasks.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import schemas
 
 router = APIRouter(
     prefix="/tasks",
@@ -8,3 +9,12 @@ router = APIRouter(
 @router.get("/{user_id}")
 async def get_user_tasks(user_id: str):
     return {"uid": user_id}
+
+# Create a task
+@router.post("/", response_model=schemas.TaskCreate)
+def create_task(task_data: schemas.TaskCreate): # Add the db parameter here, I only included schema
+    """
+    Creates a new task and assigns it to a user.
+    """
+    # CRUD function for database operations
+    return True
---
+++ backend/schemas.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel, Field
+from datetime import date, time
+
+class TaskCreate(BaseModel):
+    user_id: int = Field(..., title="User ID", description="The ID of the user assigned to the task")
+    task_id: int = Field(..., title="Task ID", description="Unique task identifier")
+    task_title: str = Field(..., title="Task Name", min_length=1, max_length=255)
+    task_date: date = Field(..., title="Task Date")
