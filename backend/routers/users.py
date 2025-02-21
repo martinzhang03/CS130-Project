a5512305863d0490af8ea2337e46266f741214cb
martin <97656152+martinzhang03@users.noreply.github.com>
2025-02-21 00:47:15 -0800

    Add files via upload

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -16,21 +16,20 @@ async def register_user(user: schemas.UserEmail, db: asyncpg.Connection = Depend
     try:
         existing_user, _, _ = await crud.find_existing_user(db, user.email, "formal")
         if existing_user:
-            raise HTTPException(
-                status_code=401,
-                detail="Registration Failed: Existing User, please login or reset password.",
-            )
+            return {"status": "fail", "message": "Registration Failed: Existing User, please login or reset password."}
 
-        code = crud.send_confirmation_email(user.email)  
-        salt, hashed_password = crud.hash_info(code)
+        # code = crud.send_confirmation_email(user.email)  
+        salt, hashed_password = crud.hash_info(user.password)
         result = await crud.insert_temp_user(db, user.email, salt, hashed_password)
 
         if not result:
-            raise HTTPException(
-                status_code=500,
-                detail="Some Error Occurred, please try again",
-            )
-        return {"status": "success", "message": "Sent confirmation email"}
+        	return {"status": "fail", "message": "Some Error Occurred, please try again"}
+        user_id = await crud.user_login(db, user.email)
+        if user_id is None: 
+            return {"status": "fail", "message": "Some Error Occurred, please try again"}
+        
+        token = auth.generate_token(str(user_id))
+        return {"status": "success", "message": "User Registered", "jwt_token": token}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -44,18 +43,12 @@ async def login_user(user: schemas.UserLogin, db: asyncpg.Connection = Depends(g
         
         encrypt_code = crud.hash_info(user.password, salt)[1] if existing_user else ""
         if not existing_user or password != encrypt_code:
-            raise HTTPException(
-                status_code=401,
-                detail="Email doesn't exist or incorrect password",
-            )
-    
+        	return {"status": "fail", "message": "Email doesn't exist or incorrect password"}
+        	
         user_id = await crud.user_login(db, user.email)
         if user_id is None: 
-            raise HTTPException(
-                status_code=500,
-                detail="Some error occurred, please try again",
-            )
-        
+        	return {"status": "fail", "message": "Email doesn't exist or incorrect password"}
+            
         token = auth.generate_token(str(user_id))
         return {"status": "success", "message": "successfully logged in", "jwt_token": token}
     
