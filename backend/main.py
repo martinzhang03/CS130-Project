f53cafef0a6b0c3fb0177aab9c0b44087d9532c8
Jinyuan Zhang <martinzhang0314@gmail.com>
2025-02-24 22:11:12 -0800

    add middleware for our routers so that backend can be connected to frontend during testing.

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from schemas import *
 from database import *
 from routers import tasks
@@ -8,6 +9,14 @@ app = FastAPI(lifespan=lifespan)
 
 app.include_router(tasks.router, dependencies=[Depends(get_db)])
 app.include_router(users.router, dependencies=[Depends(get_db)])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
